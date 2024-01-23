ac67e9903ffffbc8400ea61a5a2150b64fa50211
Ntando Magana <ntandomagana@gmail.com>
2024-01-24 01:47:11 +0200

    Fixed more of JS and ID  elements
     still have to fix API because it still does not fetch the data. Weather app does not update when you enter the name of a city

--- .history/src/script_20240124012253.js
+++ .history/src/script_20240124012253.js
@@ -40,7 +40,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "92dt3dae2c3c4fo2b0226a2e5f1a0b88";
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
